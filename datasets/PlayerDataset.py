ed28c3edf2e0c7f7c94a4268613863a0b671e4eb
Theodore Glavas <thglavas@outlook.com>
2024-04-01 16:29:15 -0400

    Add player average baseline
    Modify player dataloader to use tensors, remove bad columns and normalize

--- datasets/PlayerDataset.py
+++ datasets/PlayerDataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
@@ -8,7 +9,10 @@ class PlayerDataset(Dataset):
         self.start_season = start_season
         self.stop_season = stop_season
         self.alldata = df
+        self.means = None
+        self.stds = None
         self.data = self.load_player_data(self.alldata, self.start_season, self.stop_season, self.N)
+
 
     def __len__(self):
         return len(self.data)
@@ -30,10 +34,12 @@ class PlayerDataset(Dataset):
             for season in range(start_season, stop_season - N + 2):
                 features = []
                 for s in range(season, season + N):
-                    season_data = player_data[player_data['Season'] == s]
+                    season_data = player_data.loc[player_data['Season'] == s]
+                    if season_data.shape[0] > 1:
+                        break # Skip if player has multiple entries for a season
                     if season_data.empty:
                         break
-                    features.append(season_data)
+                    features.append(torch.tensor(season_data.drop(columns=['Player', 'Season','Tm', 'Pos','S%','TOI','ATOI'],axis=1).values, dtype=torch.float32))
 
                 if len(features) != N:
                     continue
@@ -42,9 +48,25 @@ class PlayerDataset(Dataset):
                 if target.empty:
                     continue
 
-                dataset.append([features, target])
-
+                dataset.append(
+                    [
+                    torch.stack(features).reshape((len(features), len(features[0][0]))), torch.tensor(target.drop(columns=['Player', 'Season','Tm', 'Pos','S%', 'TOI','ATOI'],axis=1).values, dtype=torch.float32)[0]
+                    ])
+                
+        #normalize features and targets to zero mean and unit variance using original df
+        self.means = df.drop(columns=['Player', 'Season','Tm', 'Pos','S%','TOI','ATOI'],axis=1).values.mean(axis=0)
+        self.stds = df.drop(columns=['Player', 'Season','Tm', 'Pos','S%','TOI','ATOI'],axis=1).values.std(axis=0)
+        print(self.means)
+        print(self.stds)
+        
+        for i in range(len(dataset)):
+            dataset[i][0] = (dataset[i][0] - self.means) / self.stds
+            dataset[i][1] = (dataset[i][1] - self.means) / self.stds
+               
         return dataset
+    
+    def unnormalize(self, tensor):
+        return tensor * self.stds + self.means
 
 def get_player_dataset(file='./Data/player/processed/player_data.xlsx'):
     cols = [
